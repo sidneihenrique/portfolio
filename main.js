270dd0b3ab0768cbec0cf275a0e9d4734396a4aa
Sidnei Henrique <henrisrs@hotmail.com>
2024-01-29 23:38:33 -0300

    smooth js

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 function navSmartphone(){
     document.querySelector('nav#smartphone').classList.toggle('open');
 }
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000, // Velocidade da rolagem em milissegundos
+    speedAsDuration: true // Usa a velocidade como duração da rolagem
+});
+
+document.querySelectorAll('a[href*="#"]').forEach(function(anchor) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      console.log(scroll)  
+
+      navSmartphone();
+
+      // Obtém o destino do link interno
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      console.log(targetElement)
+
+      // Rola suavemente para o elemento de destino usando a biblioteca smooth-scroll
+      scroll.animateScroll(targetElement);
+    });
+  });
